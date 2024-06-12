9e730da67849a002c021e618551104d3a44a71b9
eduardocd18 <javierecd@gmail.com>
2024-06-12 14:30:52 -0400

    Mi CV

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+/* -- DEPENDENCIAS CSS-- */
+const { src, dest, watch, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+
+/* -- DEPENDENCIAS IMÁGENES -- */
+const cache = require("gulp-cache");
+const imagenmin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+
+/* -- DEPENDENCIAS PARA JS -- */
+const terser = require("gulp-terser-js");
+
+/* -- FUNCIONES CSS-- */
+
+/* -- Función para compilar el CSS-- */
+function css(done) {
+  src("src/scss/**/*.scss") // Identificar el archivo de sass
+    .pipe(sourcemaps.init()) // Inicia sourmaps
+    .pipe(plumber())
+    .pipe(sass()) // Compilar sass
+    .pipe(postcss([autoprefixer(), cssnano()])) // Mejora código CSS
+    .pipe(sourcemaps.write(".")) // Destino de sourcemap
+    .pipe(dest("build/css")); // Almacenar
+  done();
+}
+
+/* -- FUNCIONES PARA IMÁGENES-- */
+
+/* -- Función para reducir imágenes -- */
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3,
+  };
+
+  src("src/img/**/*.{png,jpg}") // Fuente de los archivos
+    .pipe(cache(imagenmin(opciones))) // Reducción de imágenes
+    .pipe(dest("build/img")); // Destino de las imágenes
+  done();
+}
+
+/* -- Función para convertir imágenes a webp -- */
+function versionWebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+
+  src("src/img/**/*.{png,jpg}") // Fuente de los archivos
+    .pipe(webp(opciones)) // Conversión
+    .pipe(dest("build/img")); // Guardar cambios
+  done();
+}
+
+/* -- Función para convertir imágenes a avif -- */
+function versionAvif(done) {
+  const opciones = {
+    quality: 50,
+  };
+
+  src("src/img/**/*.{png,jpg}") // Fuente de los archivos
+    .pipe(avif(opciones)) // Conversión
+    .pipe(dest("build/img")); // Guardar cambios
+  done();
+}
+
+/* -- FUNCIONES JS -- */
+
+/* -- Función para los archivos JS --  */
+function javascript(done) {
+  src("src/js/**/*.js") // Ubica la fuente del JS
+    .pipe(sourcemaps.init()) // Inicia sourcemap
+    .pipe(terser()) // Mejora el código JS
+    .pipe(sourcemaps.write(".")) // Guarda el sourmap
+    .pipe(dest("build/js")); // Guarda el archivo en destino
+
+  done();
+}
+
+/* -- FUNCIONES DE COMPILADO -- */
+
+/* -- Función para llamar al compilado -- */
+function dev(done) {
+  watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", javascript);
+  done();
+}
+
+/* -- Exportaciones o tareas ejecutables -- */
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, css, dev);
---
+++ src/js/app.js
@@ -0,0 +1,130 @@
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
+});
+
+/*  -- FUNCIÓN JS PRINCIPAL -- */
+function iniciarApp() {
+  crearGaleria();
+  scrollNav();
+  botonIrArriba();
+  navegacionFija();
+}
+
+/* -- FUNCIONES PARA IMÁGENES-- */
+
+/* -- Función para crear la galería -- */
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
+
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `<source srcset="build/img/thumb/${i}.avif" type="image/avif" />
+    <source srcset="build/img/thumb/${i}.webp" type="image/webp" /><img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galería"/>`;
+
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
+
+    galeria.appendChild(imagen);
+  }
+}
+
+/* -- Imagen en Modal-- */
+function mostrarImagen(id) {
+  const imagen = document.createElement("picture");
+  const btnArriba = document.getElementById("volverArriba");
+
+  imagen.innerHTML = `<source srcset="build/img/grande/${id}.avif" type="image/avif" />
+    <source srcset="build/img/grande/${id}.webp" type="image/webp" /><img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galería"/>`;
+
+  /* -- Crea el overlay con la imagen */
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
+  btnArriba.classList.remove("btn-volver-arriba");
+
+  /* -- Función para cerrar el overlay tocando en cualquier parte del mismo */
+  overlay.onclick = function () {
+    body.appendChild(overlay); // Agrega overlay al body
+    body.classList.remove("fijar-body"); // Quita clase al body
+    overlay.remove(); // Quita el overlay
+    btnArriba.classList.add("btn-volver-arriba");
+  };
+
+  /* -- Botón para cerrar el modal */
+  const cerrarModal = document.createElement("P");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+
+  /* -- Función para cerrar el overlay haciendo click en el botón cerrar */
+  cerrarModal.onclick = function () {
+    body.appendChild(overlay); // Agrega overlay al body
+    body.classList.remove("fijar-body"); // Quita clase al body
+    overlay.remove(); // Quita el overlay
+  };
+
+  overlay.appendChild(cerrarModal);
+
+  /* -- Añadir al HTML -- */
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
+}
+
+/* -- FUNCIONES PARA NAVEGACION --*/
+
+/* -- Función para la navegación fija -- */
+function navegacionFija() {
+  const barra = document.querySelector(".header");
+  const educacion = document.querySelector(".educacion");
+  const body = document.querySelector("body");
+
+  window.addEventListener("scroll", function () {
+    if (educacion.getBoundingClientRect().top <= 0) {
+      console.log("xd");
+      barra.classList.add("fijo");
+      body.classList.add("header-fijo"); // Aplica el padding-top cuando el header es fijo
+    } else {
+      barra.classList.remove("fijo");
+      body.classList.remove("header-fijo"); // Remueve el padding-top cuando el header no es fijo
+    }
+  });
+}
+
+/* -- Función para el scroll--  */
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const seccionScroll = e.target.attributes.href.value;
+      const seccionScroll2 = "." + seccionScroll.substr(1);
+      const seccion = document.querySelector(seccionScroll2);
+      seccion.scrollIntoView({ behaviour: "smooth" });
+    });
+  });
+}
+
+function botonIrArriba() {
+  const btnVolverArriba = document.getElementById("volverArriba");
+  const educacion = document.querySelector(".educacion");
+
+  // Verifique la posición de desplazamiento y el botón mostrar/ocultar
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY; // GObtener la posición de desplazamiento actual
+    if (educacion.getBoundingClientRect().top < 0) {
+      // Mostrar botón después de desplazarse por debajo de .sobre-mi
+      btnVolverArriba.classList.add("visible");
+    } else {
+      btnVolverArriba.classList.remove("visible");
+    }
+  });
+
+  // Agregar funcionalidad al hacer clic en el botón
+  btnVolverArriba.addEventListener("click", function () {
+    // desplácese hacia arriba sin problemas
+    document.documentElement.scrollTop = 0;
+  });
+}
