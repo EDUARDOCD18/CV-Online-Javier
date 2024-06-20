ffd63fc8b85a8f54ea1cb1f36fd17dddcc8ebf51
eduardocd18 <javierecd@gmail.com>
2024-06-20 08:57:29 -0400

    Falta la Experiencia Laboral

--- src/js/app.js
+++ src/js/app.js
@@ -4,71 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /*  -- FUNCIÓN JS PRINCIPAL -- */
 function iniciarApp() {
-  crearGaleria();
   scrollNav();
   botonIrArriba();
   navegacionFija();
-}
-
-/* -- FUNCIONES PARA IMÁGENES-- */
-
-/* -- Función para crear la galería -- */
-function crearGaleria() {
-  const galeria = document.querySelector(".galeria-imagenes");
-
-  for (let i = 1; i <= 12; i++) {
-    const imagen = document.createElement("picture");
-    imagen.innerHTML = `<source srcset="build/img/thumb/${i}.avif" type="image/avif" />
-    <source srcset="build/img/thumb/${i}.webp" type="image/webp" /><img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galería"/>`;
-
-    imagen.onclick = function () {
-      mostrarImagen(i);
-    };
-
-    galeria.appendChild(imagen);
-  }
-}
-
-/* -- Imagen en Modal-- */
-function mostrarImagen(id) {
-  const imagen = document.createElement("picture");
-  const btnArriba = document.getElementById("volverArriba");
-
-  imagen.innerHTML = `<source srcset="build/img/grande/${id}.avif" type="image/avif" />
-    <source srcset="build/img/grande/${id}.webp" type="image/webp" /><img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galería"/>`;
-
-  /* -- Crea el overlay con la imagen */
-  const overlay = document.createElement("DIV");
-  overlay.appendChild(imagen);
-  overlay.classList.add("overlay");
-  btnArriba.classList.remove("btn-volver-arriba");
-
-  /* -- Función para cerrar el overlay tocando en cualquier parte del mismo */
-  overlay.onclick = function () {
-    body.appendChild(overlay); // Agrega overlay al body
-    body.classList.remove("fijar-body"); // Quita clase al body
-    overlay.remove(); // Quita el overlay
-    btnArriba.classList.add("btn-volver-arriba");
-  };
-
-  /* -- Botón para cerrar el modal */
-  const cerrarModal = document.createElement("P");
-  cerrarModal.textContent = "X";
-  cerrarModal.classList.add("btn-cerrar");
-
-  /* -- Función para cerrar el overlay haciendo click en el botón cerrar */
-  cerrarModal.onclick = function () {
-    body.appendChild(overlay); // Agrega overlay al body
-    body.classList.remove("fijar-body"); // Quita clase al body
-    overlay.remove(); // Quita el overlay
-  };
-
-  overlay.appendChild(cerrarModal);
-
-  /* -- Añadir al HTML -- */
-  const body = document.querySelector("body");
-  body.appendChild(overlay);
-  body.classList.add("fijar-body");
 }
 
 /* -- FUNCIONES PARA NAVEGACION --*/
